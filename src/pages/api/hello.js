9cc415563cafd75f13c1d0f4a5333b6d97979dc6
unknown <joheandroid@gmail.com>
2023-09-06 17:54:26 -0300

    first commit

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const URL = 'https://talita-backend-dev-dev.up.railway.app/api/employees'
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      res.status(200).json(data)
+    }
+  )
+
+
+
 }
